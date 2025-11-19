f0b93a5514d6cecbfd57b36f642bd08b0cf0af95
boommb92-svg <boommb92@gmail.com>
2025-11-20 00:22:26 +0530

    Update update-helm-values.sh

--- jenkins/scripts/update-helm-values.sh
+++ jenkins/scripts/update-helm-values.sh
@@ -5,25 +5,21 @@ IMAGE_PRODUCT="$2"
 IMAGE_ORDER="$3"
 CHART_VALUES='charts/bluegreen-app/values.yaml'
 
-
 if [ -z "${IMAGE_WEB}" ] || [ -z "${IMAGE_PRODUCT}" ] || [ -z "${IMAGE_ORDER}" ]; then
-echo "Usage: $0 <image-web> <image-product> <image-order>"
-exit 2
+  echo "Usage: $0 <image-web> <image-product> <image-order>"
+  exit 2
 fi
 
-
-# Use python to safely update the YAML without requiring yq
 python3 - <<PY
 import sys,yaml
 f='''${CHART_VALUES}'''
 with open(f) as fh:
-d=yaml.safe_load(f)
-# set green images
+    d=yaml.safe_load(f)
 if 'app' not in d: d['app'] = {}
 d['app']['imageWebGreen'] = '${IMAGE_WEB}'
 d['app']['imageProductGreen'] = '${IMAGE_PRODUCT}'
 d['app']['imageOrderGreen'] = '${IMAGE_ORDER}'
 with open(f,'w') as fh:
-yaml.safe_dump(d,fh,default_flow_style=False)
+    yaml.safe_dump(d,fh,default_flow_style=False)
 print('Updated', f)
 PY
